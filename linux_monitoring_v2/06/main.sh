54f9dc1c80fd9b564f3271849d55b5ff79d44711
rama <i@rrmstu.ru>
2023-04-14 21:30:49 +0500

    :sparkles: :: GoAccess

---
+++ linux_monitoring_v2/06/main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# != 0 ]
+then
+    echo "Error: Invalid number of arguments (should be 0)"
+else
+    goaccess $(pwd)/../04/log/*.log --log-format=COMBINED -o report.html
+    xdg-open report.html
+fi
